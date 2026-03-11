33cb6f1a43bfe9028243f4e2ec85f760fb074b3c
Javier <J_sedanomar@uncg.edu>
2026-03-11 11:18:20 -0400

    moved files to another location

--- prisma/migrations/20250904183358_intial/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."user" (
-    "id" SERIAL NOT NULL,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
-);
